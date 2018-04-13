4b3b83768e2cc81cbff800405ccab38b381d95f7
Sergey Mitronin <mitronin.sergei@gmail.com>
2018-04-13 20:19:39 +0300

    advantages

---
+++ src/Root/Content/MainPage/Advantages/AdvantageOne/index.jsx
@@ -0,0 +1,27 @@
+import styles from './css/styles.scss';
+
+export default class extends React.Component {
+
+    constructor(props) {
+        super(props);
+
+        this.adv = props.adv;
+    }
+
+    render() {
+        return (
+            <div>
+                <div className={styles.container}>
+                    <div className={styles[`image-${this.adv.image}`]}/>
+                    <div className={styles.title}>
+                        {this.adv.title}
+                    </div>
+                    <div className={styles.content}>
+                        {this.adv.content}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+};

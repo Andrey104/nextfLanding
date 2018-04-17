58dc813b5de9503252efe503b113779d1d9695d1
Sergey Mitronin <mitronin.sergei@gmail.com>
2018-04-17 20:22:24 +0300

    add progressive

--- src/Root/Footer/index.jsx
+++ src/Root/Footer/index.jsx
@@ -5,8 +5,8 @@ export default class extends React.Component {
     render() {
         return (
             <div className={styles.container}>
-                <div className={styles.phone}>+7 (499) 229-30-39 | qwerty@mail.ru</div>
-                <div className={styles.address}>630090, г. Москва, ул. Почтовая 36а</div>
+                <div className={styles.phone}>+7 (499) 229-30-39 | info@mail.ru</div>
+                <div className={styles.address}>г. Москва, ул. Большая Почтовая 36c8</div>
                 <div className={styles.rights}>©2018 NextFormat. Все права защищены</div>
                 <div className={styles['img-container']}>
                     <div className={styles['img-1']} />

26b4953a7d210cb3ce48653ad644ae1786ea197c
Sergey Mitronin <mitronin.sergei@gmail.com>
2018-04-17 18:28:51 +0300

    add fonts

--- src/Root/Content/MainPage/Phone/index.jsx
+++ src/Root/Content/MainPage/Phone/index.jsx
@@ -5,6 +5,7 @@ export default class extends React.Component {
     render() {
         return (
             <div className={styles['container-iphone']}>
+                <div className={styles['image-load']}/>
                 <div className={styles['iphone']}/>
                 <div className={styles['title-iphone']}>Мобильные приложения</div>
                 <div className={styles['description-iphone']}>Разрабатываем приложения для всех типов устройств
